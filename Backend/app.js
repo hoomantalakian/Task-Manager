d6cce54afeb205412591e8e7cfcabc533a25e11c
Hooman Talakian <hooman.talakian@gmail.com>
2022-04-17 20:25:15 +0430

    backend error fixed

--- Backend/app.js
+++ Backend/app.js
@@ -27,5 +27,5 @@ mongoose
 // port connection   
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	console.log("Listening on port 5000");
+	console.log("Listening on port 5000"+ process.env.PORT);
 });
